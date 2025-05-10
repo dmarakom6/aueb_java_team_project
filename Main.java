d56aa35cb55367c173bdacf1c3ac53a21b40b386
Dimitris Marakomihelakis <dmarakom@gmail.com>
2025-05-10 18:33:44 +0300

    Member info

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
+/*
+			1st team project for Java Class (INF358)
+			========================================
+			MEMBERS:
+						-DIMITRIS EMMANOUIL *M*ARAKOMICHELAKIS | 3240236 | p3240126@aueb.gr
+						-VASILIS  *D*IAMANTIDIS | 3240040 | p3240040@aueb.gr
+
+
+*/
+
 
 class Main {
 	public static void main(String[] args) {
